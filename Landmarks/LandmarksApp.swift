8d389b161755021b2ecfadcd0388a4fa2896685d
Liu Shuiyuan <1210257083@qq.com>
2021-01-24 10:11:29 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by liushuiyuan on 2021/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
